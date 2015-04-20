89a06eeb95fb6c57467bf7dabb59906630152c8a
Ben Pipkin <bpipkin@uga.edu>
2015-04-20 18:39:39 -0400

    testInsertPlaylist

--- web-api-auth-examples-master/authorization_code/public/savePlaylist.php
+++ web-api-auth-examples-master/authorization_code/public/savePlaylist.php
@@ -1,16 +1,25 @@
 <?php
-$user = 'root';
-$pass = '';
-$dbh = new PDO('mysql:host=localhost;dbname=webterm', $user, $pass);
-$playlistName = "";//provided by user
-$userName = "";//get from session
-$data = $_POST["songData"];//JSON RETURNED
-$sql = "INSERT INTO playlist (name, ownername) VALUES ($playlistName, $userName)";
-$dbh->query($sql);
-for($i = 0; $i < data.response.songs.length; $i++){
-    $sql = "INSERT INTO song VALUES($data.response.songs[i].id,  $data.response.songs[i].title, $data.response.songs[i].artist_name)";
-    $dbh->query($sql);
-    $sql = "INSERT INTO track VALUES($data.response.songs[i].id, $playlistName, $userName, $i)";
-    $dbh->query($sql);
-}
+
+	$db = new PDO('mysql:host=localhost;dbname=webterm', 'root', '');
+
+	$pName = $_POST["pName"];
+	$pOwner = $_POST["pOwner"];
+
+	$sql = "INSERT INTO playlist (name, ownername) VALUES ($pName,$pOwner)";
+	echo $sql;
+	$dbh->query($sql);
+
+
+
+
+
+
+	/*
+	for($i = 0; $i < data.response.songs.length; $i++){
+	       $sql = "INSERT INTO song VALUES($data.response.songs[i].id,  $data.response.songs[i].title, $data.response.songs[i].artist_name)";
+    	       $dbh->query($sql);
+    	       $sql = "INSERT INTO track VALUES($data.response.songs[i].id, $playlistName, $userName, $i)";
+    	       $dbh->query($sql);
+	 }
+	 */
 ?>
